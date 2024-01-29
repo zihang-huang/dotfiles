42ce1ecd704ea3f81d8c6b22ff684927f0af5647
ermaolaoye <ermaolaoye233@gmail.com>
2024-01-29 18:21:25 +0000

    Added Snippets

--- fish/config.fish
+++ fish/config.fish
@@ -14,3 +14,21 @@ thefuck --alias | source
 export LANG=en_US.UTF-8
 
 export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+
+set -gx PATH "$HOME/.cargo/bin" $PATH;
+
+fish_vi_key_bindings
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/huangzihang/miniconda3/bin/conda
+    eval /Users/huangzihang/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/Users/huangzihang/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/huangzihang/miniconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/Users/huangzihang/miniconda3/bin" $PATH
+    end
+end
+# <<< conda initialize <<<
+
