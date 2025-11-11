84f47a0e171a454e94d22b6045360f0ce4679d1a
Zihang Huang <56563171+ermaolaoye@users.noreply.github.com>
2025-11-11 16:01:30 +0800

    Update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -12,12 +12,53 @@ config.font = wezterm.font_with_fallback({ "Cascadia Code NF", "Noto Sans SC" })
 config.font_size = 15
 
 -- Tab Bar
-config.use_fancy_tab_bar = true
 config.tab_bar_at_bottom = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
+
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+
+tabline.setup({
+	options = {
+		icons_enabled = true,
+		theme = "Dracula",
+		tabs_enabled = true,
+		theme_overrides = {},
+		section_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+		component_separators = {
+			left = wezterm.nerdfonts.pl_left_soft_divider,
+			right = wezterm.nerdfonts.pl_right_soft_divider,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.pl_left_hard_divider,
+			right = wezterm.nerdfonts.pl_right_hard_divider,
+		},
+	},
+	sections = {
+		tabline_a = { "mode" },
+		tabline_b = { "workspace" },
+		tabline_c = { " " },
+		tab_active = {
+			"index",
+			{ "parent", padding = 0 },
+			"/",
+			{ "cwd", padding = { left = 0, right = 1 } },
+			{ "zoomed", padding = 0 },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+		tabline_x = {},
+		tabline_y = {},
+		tabline_z = { "domain" },
+	},
+	extensions = {},
+})
+
+tabline.apply_to_config(config)
 
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 0
 
 config.window_decorations = "RESIZE"
 config.window_padding = {
